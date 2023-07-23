17fb9a0a679ad4139b3c25a9979f762a328de2f7
Dhruv-Coder284 <dhruvgoel_me21b14_14@dtu.ac.in>
2023-07-23 12:59:10 +0530

    new

--- app/src/main/java/com/example/app1/LoginPage.kt
+++ app/src/main/java/com/example/app1/LoginPage.kt
@@ -24,8 +24,9 @@ class LoginPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityLoginPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar!!.hide()
+        binding.button.setOnClickListener {R.style.Base_Theme_AppCompat_Light_App1  }
         var switcher = binding.switch1
+
         var sharedPreferences = getSharedPreferences("MODE", MODE_PRIVATE)
         nightMODE = sharedPreferences.getBoolean("night", false)
         if (nightMODE) {
@@ -43,55 +44,20 @@ class LoginPage : AppCompatActivity() {
                var editor = sharedPreferences.edit()
                 editor.putBoolean("night", true)
                 editor.apply()
-
             }
-
         }
         )
-        binding.SystemDefault.setOnClickListener {setTheme(R.style.Base_Theme_app1)
-        }
-
-
-        binding.map.setOnClickListener{
+        binding.maps.setOnClickListener{
             var intent = Intent(this,maps::class.java)
             startActivity(intent)
         }
-        binding.car.setOnClickListener{
+        binding.jimny.setOnClickListener{
             var intent = Intent(this,Model::class.java)
             startActivity(intent)
         }
-        fun buttonSendEmail(view: View?) {
-            try {
-                val stringSenderEmail = "ninjahatori2804@gmail.com"
-                val stringReceiverEmail = "dhruv.goel2804@gmail.com"
-                val stringPasswordSenderEmail = "Vadk@2818"
-                val stringHost = "smtp.gmail.com"
-                val properties = System.getProperties()
-                properties["mail.smtp.host"] = stringHost
-                properties["mail.smtp.port"] = "465"
-                properties["mail.smtp.ssl.enable"] = "true"
-                properties["mail.smtp.auth"] = "true"
-                val session = Session.getInstance(properties, object : Authenticator() {
-                    override fun getPasswordAuthentication(): PasswordAuthentication {
-                        return PasswordAuthentication(stringSenderEmail, stringPasswordSenderEmail)
-                    }
-                })
-                val mimeMessage = MimeMessage(session)
-                mimeMessage.addRecipients(Message.RecipientType.TO, stringReceiverEmail)
-                mimeMessage.subject = "Android Test"
-                mimeMessage.setText("Hello")
-                val thread = Thread {
-                    try {
-                        Transport.send(mimeMessage)
-                    } catch (e: MessagingException) {
-                        throw RuntimeException(e)
-                    }
-                }
-                thread.start()
-                Transport.send(mimeMessage)
-            } catch (e: MessagingException) {
-                throw RuntimeException(e)
-            }
+        binding.button2.setOnClickListener{
+            var intent = Intent(this, feedbackform::class.java)
+            startActivity(intent)
         }
 
 
